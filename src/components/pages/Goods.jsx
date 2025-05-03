7b313e06abb52a9c43b5e02109ff568e859e6d23
zvezd <adelevnikita@gmail.com>
2025-05-03 18:10:05 +0300

    fix bug with filters

--- src/components/pages/Goods.jsx
+++ src/components/pages/Goods.jsx
@@ -1,9 +1,21 @@
+import { useEffect } from "react";
 import AboutSecond from "../AboutSecond/AboutSecond";
 import CoffeeList from "../CofeeList/CoffeeList";
 import Footer from "../Footer/Footer";
 import HeaderSecond from "../HeaderSecond/HeaderSecond";
+import {
+  setCurrentFilter,
+  setSearchValue,
+} from "../../redux/filterSlice/filterSlice";
+import { useDispatch } from "react-redux";
 
 const Goods = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setCurrentFilter(""));
+    dispatch(setSearchValue(""));
+  }, []);
   return (
     <>
       <HeaderSecond page="goods" text="goods" />
