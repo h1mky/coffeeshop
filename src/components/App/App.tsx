3a75a38e4d6023132010013973321f0d5a4414b1
John Doe <adelevnikita@gmail.com>
2025-05-26 16:22:18 +0300

    add 404 page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,8 @@ import { Provider } from "react-redux";
 import { Suspense, lazy } from "react";
 import { store } from "../../redux/store";
 
+import NotFound404 from "../pages/NotFound404/NotFound404";
+
 const MainPage = lazy(() => import("../pages/MainPage"));
 const OurCoffee = lazy(() => import("../pages/OurCoffee"));
 const Goods = lazy(() => import("../pages/Goods"));
@@ -33,6 +35,7 @@ const App = () => {
             <Route path="/coffee" element={<OurCoffee />} />
             <Route path="goods" element={<Goods />} />
             <Route path="/coffee/:id" element={<CoffeePage />} />
+            <Route path="*" element={<NotFound404 />} />
           </Routes>
         </Suspense>
       </Router>
